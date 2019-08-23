792c9fd7eaa9123ff99666ab94fe6317b463a563
liuhaoran <lhrmachinery@163.com>
2019-08-23 13:16:18 +0800

    add func signature

--- main.py
+++ main.py
@@ -12,6 +12,12 @@ class DemoApp(App):
     def build(self):
         return Builder.load_file("/Users/helldealer/offline-wallet/main.kv")
 
+    def signature(self):
+        s = '"8","coinexdex-test1","200000000","cet","1000000","create gte order",1,"market/MsgCreateOrder","10000",' \
+            '2,"61",1,543973922",coinex1x6rhu5m53fw8qgpwuljauaptvxyur57zym4jly","5945",2,3","abc/cet","5945" '
+        print s.split(',')
+        return 'sig'
+
 
 if __name__ == '__main__':
     DemoApp().run()
