3d1d29a5c3e5e0cddda217024da6fbfa4f12377a
liuhaoran <lhrmachinery@163.com>
2019-08-23 15:35:20 +0800

    function: key generate & list

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import sys
+
 from kivy.app import App
 from kivy.lang import Builder
 
 import os
-
+import pexpect
 
 MODULE_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 
@@ -12,11 +14,38 @@ class DemoApp(App):
     def build(self):
         return Builder.load_file("/Users/helldealer/offline-wallet/main.kv")
 
-    def signature(self):
-        s = '"8","coinexdex-test1","200000000","cet","1000000","create gte order",1,"market/MsgCreateOrder","10000",' \
-            '2,"61",1,543973922",coinex1x6rhu5m53fw8qgpwuljauaptvxyur57zym4jly","5945",2,3","abc/cet","5945" '
-        print s.split(',')
+    def signature(self, s):
+        self.build_sig(s)
+
         return 'sig'
+
+    def build_sig(self, s):
+        pass
+
+    # todo: repeat senario
+    def add_key(self, name, password):
+        child = pexpect.spawnu('cetcli keys add ' + name)
+        child.logfile = sys.stdout
+        child.expect('Enter a passphrase to encrypt your key to disk:')
+        child.sendline(password)
+        child.expect('Repeat the passphrase:')
+        child.sendline(password)
+        child.expect('name')
+        child.close()
+
+    # todo: empty key senario
+    def get_key(self, name):
+        child = pexpect.spawnu('cetcli keys show ' + name)
+        child.logfile = sys.stdout
+        child.expect('name')
+        child.close()
+
+    # todo: empty keys senario
+    def list_keys(self):
+        child = pexpect.spawnu('cetcli keys list')
+        child.logfile = sys.stdout
+        child.expect('name')
+        child.close()
 
 
 if __name__ == '__main__':
