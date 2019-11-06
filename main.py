a0c5526b1bd8926e914b1ec9b63be4b04a8b5a8a
liuhaoran <lhrmachinery@163.com>
2019-11-06 11:04:40 +0800

    Delete msg_table

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import ctypes
 import sys
 
 from kivy.app import App
@@ -6,7 +7,6 @@ from kivy.properties import StringProperty
 import pexpect
 import pyqrcode
 import os
-
 
 MODULE_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 
@@ -50,35 +50,30 @@ class DemoApp(App):
         return Keys()
 
     def signature(self, s):
-        unsigned_str = self.build_sig(s)
-        signed_str = self.sign(unsigned_str)
+        print(s)
+        signed_str = self.sign("dd", "12345678", s[2:len(s)-1])
         self.create_qrcode(signed_str, '1.png')
         return signed_str
 
-    def build_sig(self, s):
-        return 'un-signature'
-
-    def sign(self, s):
-        return 'show signature output'
+    def sign(self, key, password, s):
+        lib = ctypes.CDLL('./wallet.so')
+        sign = lib.Sign
+        sign.restype = ctypes.c_char_p
+        signature = sign(key, password, s)
+        return signature.decode('utf-8')
 
     # todo: repeat senario
     def add_key(self, name, password):
-        child = pexpect.spawnu('cetcli keys add ' + name)
-        child.logfile = open('mylog.txt', 'w')
-        child.expect('Enter a passphrase to encrypt your key to disk:')
-        child.sendline(password)
-        child.expect('Repeat the passphrase:')
-        child.sendline(password)
-        child.expect('name')
-        child.close()
-        f = open('mylog.txt', 'r')
-        s = f.readlines()[5:9]
-        self.key_resp = ''.join(s)
-        f.close()
-        os.remove('mylog.txt')
-        os.remove('2.log')
-        self.create_qrcode(str(s), '2.png')
-
+        lib = ctypes.CDLL('./wallet.so')
+        lib.BearInit('tmp'.encode("utf-8"))
+        create_key = lib.CreateKey
+        create_key.restype = ctypes.c_char_p
+        key = create_key(name, password, "", 0, 0)
+        print(key)
+        s = key.decode('utf-8')
+        print(type(s))
+        self.key_resp = s
+        self.create_qrcode(s, '2.png')
 
     # todo: empty key senario
     def get_key(self, name):
@@ -89,14 +84,14 @@ class DemoApp(App):
 
     # todo: empty keys senario
     def list_keys(self):
-        child = pexpect.spawnu('cetcli keys list')
-        child.logfile = open('mylog.txt', 'w')
-        child.expect('name')
-        child.close()
-        f = open('mylog.txt', 'r')
-        self.keys_list = ''.join(f.readlines())
-        f.close()
-        os.remove('mylog.txt')
+        lib = ctypes.CDLL('./wallet.so')
+        print(lib)
+        list_keys = lib.ListKeys
+        print(list_keys)
+        list_keys.restype = ctypes.c_char_p
+        keys = list_keys()
+        print(keys)
+        self.keys_list = keys.decode('utf-8')
 
     def create_qrcode(self, s, path):
         url = pyqrcode.create(s, error='L')
